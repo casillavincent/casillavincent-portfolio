83266aa4ca81897c64f3d7236c8dcdb84353dd7e
Vincent Casilla <73447863+mikanunu@users.noreply.github.com>
2021-02-11 14:24:39 -0800

    Development files fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 function App() {
    return (
       <React.Fragment>
-         <h1>React Start</h1>
+         <h1>Portfolio Starter</h1>
       </React.Fragment>
    );
 }
